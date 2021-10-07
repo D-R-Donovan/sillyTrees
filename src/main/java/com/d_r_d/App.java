aff010f8473306aaa6098c3a19ff081079c99181
D-R-Donovan <dylan.r.donovan@gmail.com>
2021-10-07 15:58:57 +0100

    added maven project managment

---
+++ src/main/java/com/d_r_d/App.java
@@ -0,0 +1,19 @@
+package com.d_r_d;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args){
+        LinklessTree<Integer> tree = new LinklessTree<>(17);
+        for (int i = 0; i < 100; i++) {
+            tree.insert(i);
+        }
+        System.out.println(tree.contains(50));
+        System.out.println(tree.findIndex(42));
+        System.out.println(tree.delete(21));
+    }
+}
+
